f8aacff4dbfe98e09daf0ed9ca889b415f6b4c2b
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-04-17 18:14:07 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+
+import './App.css';
+
+function App() {
+
+    const goodList = [
+        {
+            name:'Кроссовки 1',
+            price:'1000р',
+            brand:'Adidas',
+        },
+        {
+            name:'Кроссовки 2',
+            price:'2000р',
+            brand:'Nike',
+        },
+        {
+            name:'Кроссовки 3',
+            price:'3000р',
+            brand:'Adidas',
+        },
+        {
+            name:'Кроссовки 4',
+            price:'999р',
+            brand:'Asics',
+        },
+        {
+            name:'Кроссовки 5',
+            price:'1500р',
+            brand:'Puma',
+        },
+        {
+            name:'Кроссовки 6',
+            price:'1250р',
+            brand:'Adidas',
+        },
+
+    ]
+
+  return (
+     <div className="App">
+         <header className="header">
+
+             <input
+                className='search-field'
+                type='text'
+             />
+
+         </header>
+         <main className="main-wrapper" >
+             {
+                 goodList.map((item) => (
+                     <div className="item-block" >
+                         <p className='item-name'>{item.name}</p>
+                         <p className='item-name'>{item.brand}</p>
+                         <p className='item-name'>{item.price}</p>
+                     </div>
+                 ))
+             }
+
+         </main>
+
+     </div>
+  );
+}
+
+export default App;
