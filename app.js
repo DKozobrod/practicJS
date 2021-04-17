a11c0868560770d4e018ce31634daca849601319
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-04-17 18:35:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-
+import { useState } from 'react';
 import './App.css';
+
+const goodList = [
+    {
+        name:'Кроссовки 1',
+        price:'1000р',
+        brand:'Adidas',
+    },
+    {
+        name:'Кроссовки 2',
+        price:'2000р',
+        brand:'Nike',
+    },
+    {
+        name:'Кроссовки 3',
+        price:'3000р',
+        brand:'Adidas',
+    },
+    {
+        name:'Кроссовки 4',
+        price:'999р',
+        brand:'Asics',
+    },
+    {
+        name:'Кроссовки 5',
+        price:'1500р',
+        brand:'Puma',
+    },
+    {
+        name:'Кроссовки 6',
+        price:'1250р',
+        brand:'Adidas',
+    },
+
+];
 
 function App() {
 
-    const goodList = [
-        {
-            name:'Кроссовки 1',
-            price:'1000р',
-            brand:'Adidas',
-        },
-        {
-            name:'Кроссовки 2',
-            price:'2000р',
-            brand:'Nike',
-        },
-        {
-            name:'Кроссовки 3',
-            price:'3000р',
-            brand:'Adidas',
-        },
-        {
-            name:'Кроссовки 4',
-            price:'999р',
-            brand:'Asics',
-        },
-        {
-            name:'Кроссовки 5',
-            price:'1500р',
-            brand:'Puma',
-        },
-        {
-            name:'Кроссовки 6',
-            price:'1250р',
-            brand:'Adidas',
-        },
+    const [inputValue, setInputValue ] = useState('');
 
-    ]
+    const onSearchItems =(e) => {
+        setInputValue(e.target.value);
+    };
 
   return (
      <div className="App">
@@ -44,6 +50,8 @@ function App() {
              <input
                 className='search-field'
                 type='text'
+                value={inputValue}
+                onChange={onSearchItems}
              />
 
          </header>
