3c37933c6a71d861fa48ffb570834aa07bfb71bf
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-04-24 10:03:05 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+
+import Header from './header';
+import './App.css';
+
+const goodList = [
+    {
+        name:'Кроссовки 1',
+        price:'1000р',
+        brand:'Adidas',
+    },
+    {
+        name:'Кроссовки 2',
+        price:'2000р',
+        brand:'Nike',
+    },
+    {
+        name:'Кроссовки 3',
+        price:'3000р',
+        brand:'Adidas',
+    },
+    {
+        name:'Кроссовки 4',
+        price:'999р',
+        brand:'Asics',
+    },
+    {
+        name:'Кроссовки 5',
+        price:'1500р',
+        brand:'Puma',
+    },
+    {
+        name:'Кроссовки 6',
+        price:'1250р',
+        brand:'Adidas',
+    },
+
+];
+
+function App() {
+
+    const [inputValue, setInputValue] = useState('');
+    const [goods, setGoods] = useState([]);
+
+    const onSearchItems =(e) => { 
+      
+      const { value } = e.target;
+        setInputValue(value);
+
+
+        const newItems = goodList.filter(( { name } ) => 
+name.toLowerCase().includes(value.toLowerCase())
+        )
+
+        setGoods(newItems);
+        console.log(newItems);
+    };
+
+    useEffect(() => {
+      setGoods(goodList);
+    },[]);
+
+
+  return (
+     <div className="App">
+       <Header 
+          inputValue={inputValue}
+          onSearchItems={onSearchItems}
+       />
+         <main className="main-wrapper" >
+             {
+                 goods.map((item) => (
+                     <div className="item-block" >
+                         <p className='item-name'>{item.name}</p>
+                         <p className='item-name'>{item.brand}</p>
+                         <p className='item-name'>{item.price}</p>
+                     </div>
+                 ))
+             }
+
+         </main>
+
+     </div>
+  );
+}
+
+export default App;
