a8f58483bcd2dbaa7f67d41cbea372cd2a7ebd2d
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-04-24 10:03:48 +0300

    Create header.js

---
+++ header.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+
+function Header({ inputValue , onSearchItems}) {
+
+
+  return (
+     <>
+         <header className="header">
+             <input
+                className='search-field'
+                type='text'
+                value={inputValue}
+                onChange={onSearchItems}
+             />
+
+         </header>
+     </>
+  );
+}
+
+export default Header;
